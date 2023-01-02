1f6c4dbffb43a98cf2e9e397e5bf7080734f34e6
Taeangel <wngks2050@gmail.com>
2023-01-02 16:56:06 +0900

    refactor: OpenMarketNetwork_Test unitTest 완료

--- OpenMarket/OpenMarketNetwork_Test/OpenMarketNetwork_Test.swift
+++ OpenMarket/OpenMarketNetwork_Test/OpenMarketNetwork_Test.swift
@@ -14,14 +14,17 @@ final class OpenMarketNetwork_Test: XCTestCase {
   var cancellable = Set<AnyCancellable>()
   var product: ProductModel? = nil
   var productList: [Product] = []
-  var openMarketNetwork: ApiManager!
+  var mockSession: Requestable!
+  var mockFailSession: Requestable!
 
   override func setUpWithError() throws {
-    openMarketNetwork = ApiManager(session: URLSession.shared)
+    mockSession = MockSession()
+    mockFailSession = MockFailSession()
   }
   
   override func tearDownWithError() throws {
-    openMarketNetwork = nil
+    mockSession = nil
+    mockFailSession = nil
     product = nil
     productList = []
     cancellable.removeAll()
@@ -29,6 +32,7 @@ final class OpenMarketNetwork_Test: XCTestCase {
   
   func test_ApiManager_requestPublisherGetProduct_shouldBeWork() {
     //when
+    let openMarketNetwork = ApiManager(session: URLSession.shared)
     let expectation = XCTestExpectation(description: "개별조회")
     let id = 10
     //then
@@ -45,6 +49,7 @@ final class OpenMarketNetwork_Test: XCTestCase {
   
   func test_ApiManager_requestPublisherGetProductList_shouldBeWork() {
     //when
+    let openMarketNetwork = ApiManager(session: URLSession.shared)
     let expectation = XCTestExpectation(description: "리스트 조회")
     let page_no = 1
     let items_per_page = 20
@@ -61,8 +66,11 @@ final class OpenMarketNetwork_Test: XCTestCase {
     XCTAssertEqual(productList.count, 20)
   }
   
+  
+  //이테스트는 테스트를 할 때마다 openData를 오염시키는 문제가 발생함
   func test_ApiManager_requestPublisherPostProduct_shouldBeWork() {
     //when
+    let openMarketNetwork = ApiManager(session: URLSession.shared)
     let expectation = XCTestExpectation(description: "포스트 조회")
 
     let product = ProductEncodeModel(name: "테스트입니다아아아아아",
@@ -87,8 +95,10 @@ final class OpenMarketNetwork_Test: XCTestCase {
     XCTAssertEqual(product.price, 10)
   }
   
+  //이테스트는 테스트를 할 때마다 openData를 오염시키는 문제가 발생함
   func test_ApiManager_requestPublishermodifyProduct_shouldBeWork() {
     //when
+    let openMarketNetwork = ApiManager(session: URLSession.shared)
     let expectation = XCTestExpectation(description: "포스트 조회")
 
     let product = ProductEncodeModel(name: "테스트입니다아아아아아",
@@ -111,28 +121,114 @@ final class OpenMarketNetwork_Test: XCTestCase {
     XCTAssertEqual(product.price, 10)
   }
   
-//  func test_ApiManager_requestPublishermodifyProduct_shouldBeWork() {
-//    //when
-//    let expectation = XCTestExpectation(description: "포스트 조회")
-//
-//    let product = ProductEncodeModel(name: "테스트입니다아아아아아",
-//                                     description: "설명이다아아아아아아",
-//                                     price: 10,
-//                                     currency: "KRW",
-//                                     discountedPrice: 5,
-//                                     stock: 5)
-//
-//    //then
-//    ApiManager.shared.requestPublisher(.productDeletionURISearch(id: <#T##Int#>))
-//      .receive(on: DispatchQueue.main)
-//      .sink(receiveCompletion: ApiManager.shared.handleCompletion) { [weak self] data in
-//        self?.product = try? JSONDecoder().decode(ProductModel.self, from: data)
-//        expectation.fulfill()
-//      }
-//      .store(in: &cancellable)
-//    //given
-//    wait(for: [expectation], timeout: 2)
-//    XCTAssertEqual(product.price, 10)
-//  }
-//
+  func test_ApiManager_requestPublisher_getProductList_shouldBeWork() {
+    //when
+    let openMarketNetwork = ApiManager(session: mockSession)
+    let expectation = XCTestExpectation(description: "getProductList 조회")
+    let page_no = 1
+    let items_per_page = 20
+    //then
+    openMarketNetwork.requestPublisher(.getProductList(page_no: page_no, items_per_page: items_per_page))
+      .sink(receiveCompletion: openMarketNetwork.handleCompletion) { result in
+        expectation.fulfill()
+      }
+      .store(in: &cancellable)
+    
+    //given
+    wait(for: [expectation], timeout: 2)
+  }
+  
+  func test_ApiManager_requestPublisher_getProduct_shouldBeWork() {
+    //when
+    let openMarketNetwork = ApiManager(session: mockSession)
+    let expectation = XCTestExpectation(description: "getProduct 조회")
+
+    //then
+    openMarketNetwork.requestPublisher(.getProduct(24))
+      .sink(receiveCompletion: openMarketNetwork.handleCompletion) { result in
+        expectation.fulfill()
+      }
+      .store(in: &cancellable)
+    
+    //given
+    wait(for: [expectation], timeout: 2)
+  }
+  
+  func test_ApiManager_requestPublisher_productDeletionURISearch_shouldBeWork() {
+    //when
+    let openMarketNetwork = ApiManager(session: mockSession)
+    let expectation = XCTestExpectation(description: "productDeletionURISearch 조회")
+
+    //then
+    openMarketNetwork.requestPublisher(.productDeletionURISearch(id: 14))
+      .sink(receiveCompletion: openMarketNetwork.handleCompletion) { result in
+        expectation.fulfill()
+      }
+      .store(in: &cancellable)
+    
+    //given
+    wait(for: [expectation], timeout: 2)
+  }
+  
+  func test_ApiManager_requestPublisher_deleteProduct_shouldBeWork() {
+    //when
+    let openMarketNetwork = ApiManager(session: mockSession)
+    let expectation = XCTestExpectation(description: "productDeletionURISearch 조회")
+
+    //then
+    openMarketNetwork.requestPublisher(.productDeletionURISearch(id: 25))
+      .sink(receiveCompletion: openMarketNetwork.handleCompletion) { data in
+        guard let deleteURL = String(data: data, encoding: .utf8) else { return }
+        openMarketNetwork.requestPublisher(.deleteProduct(endpoint: deleteURL))
+          .sink(receiveCompletion: openMarketNetwork.handleCompletion) { result in
+            expectation.fulfill()
+          }
+      }
+      .store(in: &cancellable)
+    
+    //given
+    wait(for: [expectation], timeout: 2)
+  }
+  
+  func test_ApiManager_requestPublisher_modifyProduct_shouldBeWork() {
+    //when
+    let openMarketNetwork = ApiManager(session: mockSession)
+    let expectation = XCTestExpectation(description: "modifyProduct 조회")
+    let product = ProductEncodeModel(name: "목객체", description: "입니다아아아", price: 100, currency: "USD", discountedPrice: 10, stock: 10)
+
+    //then
+    openMarketNetwork.requestPublisher(.modifyProduct(id: 14, product: product))
+      .sink(receiveCompletion: openMarketNetwork.handleCompletion) { result in
+        expectation.fulfill()
+      }
+      .store(in: &cancellable)
+    
+    //given
+    wait(for: [expectation], timeout: 2)
+  }
+  
+  func test_ApiManager_requestPublisher_modifyProduct_shouldBeFail() {
+    //when
+    let openMarketNetwork = ApiManager(session: mockFailSession)
+    let expectation = XCTestExpectation(description: "modifyProduct 조회")
+
+    //then
+    openMarketNetwork.requestPublisher(.getProduct(14))
+      .sink(receiveCompletion: { completion in
+        switch completion {
+        case .finished:
+          break
+        case .failure:
+          expectation.fulfill()
+        }
+      }, receiveValue: { result in
+        XCTFail()
+      })
+      .store(in: &cancellable)
+    
+    //given
+    wait(for: [expectation], timeout: 2)
+  }
+  
 }
+
